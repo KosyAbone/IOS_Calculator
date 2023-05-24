e8d6c740cbc7ad92bc37f1edf61372de513a7290
Kosiso Abone <abonekosiso@gmail.com>
2023-05-24 11:25:47 -0400

    Initial Commit

---
+++ Kosiso_Abone_Red_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kosiso_Abone_Red_Calculator
+//
+//  Created by KOSSY on 2023-05-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
